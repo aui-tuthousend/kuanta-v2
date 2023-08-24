72eb12035c24b0128e9cf36d84d024a35a879bde
aui_tuthousend <mraihan286@gmail.com>
2023-08-24 10:02:21 +0700

    adding bootsrap laravel & jetsream

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,17 @@ Route::get('/allkpis');
 Route::get('/kpisrole/{role}')->name('kpisrole');
 
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
